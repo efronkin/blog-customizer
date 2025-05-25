de502992cf29f3a486b8931809cdeca6642e013e
Eugene Fronkin <efronkin@gmail.com>
2025-05-25 17:30:49 +0300

    Доработки по первому ревью, убраны комментарии из index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,31 +8,6 @@ import './styles/index.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-// const App = () => {
-// 	const [articleState, setArticleState]
-// 		= useState<ArticleStateType>(defaultArticleState);
-
-// 	return (
-// 		<main
-// 			className={styles.main}
-// 			style={
-// 				{
-// 					'--font-family': articleState.fontFamilyOption.value,
-// 					'--font-size': articleState.fontSizeOption.value,
-// 					'--font-color': articleState.fontColor.value,
-// 					'--container-width': articleState.contentWidth.value,
-// 					'--bg-color': articleState.backgroundColor.value,
-// 				} as CSSProperties
-// 			}>
-// 			<ArticleParamsForm
-// 				articleState={articleState}
-// 				setArticleState={setArticleState}
-// 			/>
-// 			<Article />
-// 		</main>
-// 	);
-// };
-
 root.render(
 	<StrictMode>
 		<App />
